d3fbe1a908001e98834633a1832a9e4f912ad9b3
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-19 00:41:26 -0500

    index.php

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ require_once ('vendor/autoload.php');
 $f3 = Base::instance();
 //Set debug level
 $f3->set('DEBUG',3);
+//start session
+session_start();
+
 
 //define a default route
 $f3->route('GET /',
@@ -30,19 +33,28 @@ $f3->route('GET /show/@animal', function($f3,$params){
             echo "no";
             }});
 
-$f3-> route('GET /order',function(){
+$f3-> route('GET /order',function($f3,$params){
+
+          $f3-> set('animal',$params['animal']);
+           $_SESSION['animal'] = $f3-> get('animal');
            $template = new Template();
            echo $template->render('views/form1.html');
+
 });
 
-$f3-> route('GET /order2',function(){
+$f3-> route('POST /order2',function($f3,$params){
+    $f3-> set('color',$params['color']);
+    $_SESSION['color'] = $f3-> get('color');
     $template = new Template();
-    echo $template->render('views/form2.html');
+   echo $template->render('/views/form2.html');
+
 });
 
-$f3-> route('GET /order',function(){
+$f3-> route('POST /result',function($f3,$params){
+    $_SESSION['animal'] = $f3-> get('animal');;
+    $_SESSION['color'] = $f3-> get('color');;
     $template = new Template();
-    echo $template->render('views/results.html');
+    echo $template->render('/views/result.html');
 });
 //run fat free
 $f3->run();
