fdd5dafbd8028352e4621492ae31fb9442ed6ad8
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-25 18:58:48 -0500

    new i

--- index.php
+++ index.php
@@ -6,53 +6,75 @@
  * Time: 4:05 PM
  */
 //require the autolaod file
-require_once ('vendor/autoload.php');
+require_once('vendor/autoload.php');
 //create an instance of the base class
 $f3 = Base::instance();
 //Set debug level
-$f3->set('DEBUG',3);
+$f3->set('DEBUG', 3);
 //start session
 session_start();
 
 
 //define a default route
 $f3->route('GET /',
-    function() {
-      $template= new Template();
+    function () {
+        $template = new Template();
         echo $template->render('views/home.html');
     });
 
-$f3->route('GET /show/@animal', function($f3,$params){
-    switch ($params['animal']){
+$f3->route('GET /show/@animal', function ($f3, $params) {
+    switch ($params['animal']) {
         case 'cat';
-            echo "<img src='/328/pet2/images/cat.jpg'>"; break;
+            echo "<img src='/328/pet2/images/cat.jpg'>";
+            break;
         case 'dog';
-            echo "<img src='/328/pet2/images/dog.png'>"; break;
+            echo "<img src='/328/pet2/images/dog.png'>";
+            break;
         default:
-          //  $f3->error(404);
+            //  $f3->error(404);
             echo "no";
-            }});
+    }
+});
 
-$f3-> route('GET /order',function($f3,$params){
-          $template = new Template();
-           echo $template->render('views/form1.html');
+$f3->route('GET /order', function ($f3, $params) {
+    $template = new Template();
+    echo $template->render('views/form1.html');
 
 });
 
-$f3-> route('POST /order2',function($f3,$params){
-    $_SESSION['animal']= $_POST['animal'];
-   $template = new Template();
-   echo $template->render('/views/form2.html');
+$f3->route('POST /order2', function ($f3, $params) {
+    $_SESSION['animal'] = $_POST['animal'];
+    $template = new Template();
+    echo $template->render('/views/form2.html');
 
 });
 
-$f3-> route('POST /result',function($f3,$params){
+$f3->route('POST /result', function ($f3, $params) {
     $_SESSION['color'] = $_POST['color'];
-    $f3-> set('animal',$_SESSION['animal'] );
-    $f3-> set('color', $_SESSION['color']);
+    $f3->set('animal', $_SESSION['animal']);
+    $f3->set('color', $_SESSION['color']);
     $template = new Template();
     echo $template->render('/views/result.html');
 });
+
+$f3->set('colors', array('pink', 'green', 'blue'));
+$f3->route('GET|POST /new-pet',
+    function ($f3) {
+    include('model/validate.php');
+        if (isset($_POST['submit'])) {
+            $color = $_POST['pet-color'];
+            $petname = $_POST['pet-name'];
+            $pettype = $_POST['pet-type'];
+        }
+        $f3->set('color',$color);
+        $f3->set('pet-name',$petname);
+        $f3->set('pet-type',$pettype);
+        //$f3->set('errors',$errors);
+        //$f3->set('success',$success);
+
+        $template = new Template();
+        echo $template->render('views/new-pet.php');
+    });
 //run fat free
 $f3->run();
 ?>
