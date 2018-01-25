e029225f3522dbc77dee658834aeaeff39a15217
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-25 18:26:10 -0500

    boot  add

--- views/new-pet.php
+++ views/new-pet.php
@@ -3,18 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Pair 2</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
 </head>
 <body>
-<h2> Order a Pet</h2>
+<div class="container">
+
 <form action="#" method="post">
-Pet Name:<input type="text">
-Pet Color <select name="pet-color" id="pet-color">
+    <fieldset>
+    <legend> Order a Pet</legend>
+        <label>Pet Name:<input type="text"></label>
+<label>Pet Color <select name="pet-color" id="pet-color">
         <repeat group="{{ @colors }}" value ="{{@colorOption}}">
             <option>{{@colorOption}}</option>
         </repeat>
     </select>
-    Pet Type:<input type="text"><br><br>
+</label>
+        <label>
+            Pet Type:<input type="text"></label>
     <input type="button" value=" Submit">
+    </fieldset>
 </form>
+</div>
 </body>
 </html>
