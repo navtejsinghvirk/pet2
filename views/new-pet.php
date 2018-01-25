07676bf8ba3aae87e4c1f3b17238f6a7729f9e1f
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-25 18:02:02 -0500

    new-pet.php

---
+++ views/new-pet.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair 2</title>
+</head>
+<body>
+<h2> Order a Pet</h2>
+<form action="#" method="post">
+Pet Name:<input type="text">
+Pet Color <select name="color">
+        <option>white</option>
+        <option>green</option>
+    </select>
+    Pet Type:<input type="text"><br><br>
+    <input type="button" value=" Submit">
+</form>
+</body>
+</html>
