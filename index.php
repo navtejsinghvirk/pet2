23e40096fc19aa8546f861b5f2bccdbe932b65ce
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-25 21:27:45 -0500

    new update

--- index.php
+++ index.php
@@ -57,20 +57,25 @@ $f3->route('POST /result', function ($f3, $params) {
     echo $template->render('/views/result.html');
 });
 
+
 $f3->set('colors', array('pink', 'green', 'blue'));
+
 $f3->route('GET|POST /new-pet',
     function ($f3) {
-    include('model/validate.php');
         if (isset($_POST['submit'])) {
+            include('model/validate.php');
             $color = $_POST['pet-color'];
-            $petname = $_POST['pet-name'];
-            $pettype = $_POST['pet-type'];
+            $petName = $_POST['pet-name'];
+            $petType = $_POST['pet-type'];
+            $errors = $_POST['errors'];
+            $success = $_POST['success'];
         }
+
         $f3->set('color',$color);
-        $f3->set('pet-name',$petname);
-        $f3->set('pet-type',$pettype);
-        //$f3->set('errors',$errors);
-        //$f3->set('success',$success);
+        $f3->set('pet-name',$petName);
+        $f3->set('pet-type',$petType);
+        $f3->set('errors',$errors);
+        $f3->set('success',$success);
 
         $template = new Template();
         echo $template->render('views/new-pet.php');
